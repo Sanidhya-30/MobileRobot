4bd31beb46d4ec95c3d3664573d4467a1fc31b1a
Sanidhya-30 <sanidhya3009@gmail.com>
2024-02-10 15:50:07 +0530

    updatedrepo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/MobileRobot/src
+# Build directory: /home/ubuntu/MobileRobot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mobile_robot")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/MobileRobot/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/ubuntu/.pyenv/versions/3.10.11/bin:/home/ubuntu/.pyenv/libexec:/home/ubuntu/.pyenv/plugins/python-build/bin:/home/ubuntu/.nvm/versions/node/v20.4.0/bin:/usr/local/cuda-12.2/bin:/home/ubuntu/.pyenv/shims:/home/ubuntu/.pyenv/bin:/usr/lib/ccache:/home/ubuntu/ardupilot/Tools/autotest:/opt/gcc-arm-none-eabi-10-2020-q4-major/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.dotnet/tools'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/MobileRobot/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/ubuntu/MobileRobot/src:$ROS_PACKAGE_PATH"
