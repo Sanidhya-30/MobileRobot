755f9ff9659d4b7cc3ddadbda5d760534625a2b3
Sanidhya-30 <sanidhya3009@gmail.com>
2024-02-10 16:14:14 +0530

    updated .gitignore to exclude build and dev

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/MobileRobot/src
-# Build directory: /home/ubuntu/MobileRobot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("mobile_robot")
